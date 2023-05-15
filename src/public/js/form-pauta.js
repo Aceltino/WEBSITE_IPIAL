df85cfaf5af87f08463a368eed5e6caede7d718f
Otaniel Fernandes <113445501+otanielfernandes@users.noreply.github.com>
2023-05-15 21:24:18 +0100

    Validação para o formulário  de pautas

---
+++ src/public/js/form-pauta.js
@@ -0,0 +1,7 @@
+
+const form_pauta = document.getElementById('form-pauta');
+
+form_pauta.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    alert("Pauta cadastrada com sucesso");
+})
