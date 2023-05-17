916c4f74a5561ad9b7ea3df77e2bc3dc50aa2b67
Otaniel Fernandes <113445501+otanielfernandes@users.noreply.github.com>
2023-05-17 03:02:08 +0100

    New

--- src/public/js/form-contacto.js
+++ src/public/js/form-contacto.js
@@ -8,14 +8,9 @@ const campos = document.querySelectorAll('.required');
 const spans = document.querySelectorAll('.span-required');
 
 
-/****Criando um RegEx para os nomes*****/
-const nomeRegEx = /^([A-Z][a-z-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]{1,}) ([A-Z][a-z-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]{0,}$)/;
-
 /******Criando uma RegEx para o email*******/
 const emailRegEx = /^([a-z0-9]{2,}\.)?([a-z0-9]{2,})@([a-z0-9]{2,})(\.[a-z]{2,})(\.ao|\.gov|\.co)?/;
 
-/****Criando um ReqEx para o assunto******/
-const assuntoRegEx = /^([A-Z][a-z]{0,}.*) ([A-z]{0,}.*)/;
 
 
 //Criando um evento para que toda vez que o botão do tipo submit for criado
