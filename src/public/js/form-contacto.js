05e663e4d3104b65ca2f22a58b7b9c0f103b6b28
Otaniel Fernandes <113445501+otanielfernandes@users.noreply.github.com>
2023-05-23 17:26:23 +0100

    New

--- src/public/js/form-contacto.js
+++ src/public/js/form-contacto.js
@@ -1,44 +1,41 @@
 /*******Pegar o formulário atráves do seu ID**********/
 const form = document.getElementById('form-contacto');
 
-/*****Pegar todos os campos que possuem a class "required" ******/
-const campos = document.querySelectorAll('.required');
+/*****Pegar o campo que possui a class "required" ******/
+const campo = document.querySelectorAll('.required');
 
-/****Pegar cada span dos inputs e da text-area******/
-const spans = document.querySelectorAll('.span-required');
-
+/****Pegar o span do input-email******/
+const span = document.querySelectorAll('.span-required');
 
 /******Criando uma RegEx para o email*******/
 const emailRegEx = /^([a-z0-9]{2,}\.)?([a-z0-9]{2,})@([a-z0-9]{2,})(\.[a-z]{2,})(\.ao|\.gov|\.co)?/;
 
 //Criando uma função para adicionar o erro
-    function setError(index){
-        campos[index].style.border = '2px solid red';
-        spans[index].style.display = 'block';
+    function setError(){
+        campo.style.border = '2px solid red';
+        span.style.display = 'block';
     }
 
 //Criando uma função para remover o erro
-    function removeError(index){
-        campos[index].style.border = '2px solid #06338a';
-        spans[index].style.display = 'none';
+    function removeError(){
+        campo.style.border = '2px solid #06338a';
+        span.style.display = 'none';
     }
 
-//Criando um evento para que toda vez que o botão do tipo submit for criado
-
+//Criando um evento para que toda vez que o botão do tipo submit for clicado
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         emailValidate();
     })
 
-
-    //Criando uma funcão para validar o Email
-
+//Criando uma funcão para validar o Email
     function emailValidate(){
-        if(emailRegEx.test(campos[0].value)){
-            removeError(0);
+        if(emailRegEx.test(campo)){
+            removeError();
+            alert('Consulte o seu email, pois enviamos-lhe uma mensagem.');
         }
         else{
-            setError(0);    
+            setError();    
         }
     }
 
