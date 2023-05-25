291684766a88d03b88000709d7a872ab581713ef
Aceltino Quianvo <118996385+Aceltino@users.noreply.github.com>
2023-05-25 04:10:28 +0100

    New

--- src/Controller/InscricoesController.php
+++ src/Controller/InscricoesController.php
@@ -1,14 +1,14 @@
 <?php
-
+  require_once 'src/Model/Inscricoes.php';
  class InscricoesController{
 
     public function index()
     {
-        $resultado = Informacoes::MostrarNoticias();
+        $resultado = Inscricao::MostrarInscritos();
 
         $loader = new \Twig\Loader\FilesystemLoader('src/View');
         $twig = new \Twig\Environment($loader);
-        $template = $twig->load('noticias-admin.html');       
+        $template = $twig->load('alunos-admitidos.html');       
 
         $array = count($resultado);
         for ($i = 0; $i < $array; $i++){
